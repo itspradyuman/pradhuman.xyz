2e36728a7362b56caaa68ced1369cdb58e03e2e4
prad <pradhumansp1@gmail.com>
2022-09-06 00:17:36 +0530

    Update 404.jsx

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -4,7 +4,8 @@ import Layout from "@lekoarts/gatsby-theme-minimal-blog/src/components/layout"
 const NotFound = () => (
   <Layout>
     <h1>404 - Page Not Found</h1>
-    <p>Unfortunately we couldn't find what you were looking for :(</p>
+    <img src="https://i.redd.it/o6px8q59yw401.jpg" alt="Savage Rick and Morty"></img>
+    <p>Either you're too early here or else you found a broken link :(</p>
   </Layout>
 )
 
